617cab72aa0a3f1bbcb19c10fb748507c4d26010
Patrick Heinrich <heinrich.patrick97@web.de>
2019-11-16 14:10:48 +0100

    created CSVReader method to parse .csv to List of CSVItems

--- src/main/java/org/patrick/CSVDisplay/CSVReader.java
+++ src/main/java/org/patrick/CSVDisplay/CSVReader.java
@@ -1,5 +1,36 @@
 package org.patrick.CSVDisplay;
 
-public class CSVReader {
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class CSVReader {
+	
+    public static List<Item> csvToList(String csvFilePath) throws IOException {
+       try (
+           Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+       ) {
+           CsvToBean<Item> csvToBean = new CsvToBeanBuilder<Item>(reader)
+                   .withType(Item.class)
+                   .withSeparator(';')
+                   .build();
+
+           Iterator<Item> csvItemIterator = csvToBean.iterator();
+           List<Item> ItemList = new ArrayList<Item>();
+
+           while (csvItemIterator.hasNext()) {
+               Item item = csvItemIterator.next();
+               ItemList.add(item);
+           }
+           
+           return ItemList;           
+       }
+   }
 }
