e65edb570bc3e91665214d33145f88e4b4b78247
Patrick Heinrich <heinrich.patrick97@web.de>
2019-11-16 20:44:14 +0100

    created JFrame Table

---
+++ src/main/java/org/patrick/CSVDisplay/CSVItemTable.java
@@ -0,0 +1,24 @@
+package org.patrick.CSVDisplay;
+
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class CSVItemTable extends JFrame {
+    
+    public CSVItemTable(List<CSVItem> csvItemList) {
+        
+        CSVItemTableModel model = new CSVItemTableModel(csvItemList);
+        JTable table = new JTable(model);
+        
+        this.add(new JScrollPane(table));
+        this.setTitle("CSV Display");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+               
+    }
+
+}
