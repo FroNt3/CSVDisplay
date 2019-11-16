8a1fe811538e0042e65d28b07f5bfca986ea3ad5
Patrick Heinrich <heinrich.patrick97@web.de>
2019-11-16 12:19:01 +0100

    first commit, empty maven project

---
+++ src/main/java/org/patrick/CSVDisplay/App.java
@@ -0,0 +1,13 @@
+package org.patrick.CSVDisplay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
