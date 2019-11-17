91ac6a1adc2920d0594b36e2c6384b5572502eaf
Patrick Heinrich <heinrich.patrick97@web.de>
2019-11-17 15:39:03 +0100

    added jdoc to CSVDisplay

--- src/main/java/org/patrick/CSVDisplay/CSVDisplay.java
+++ src/main/java/org/patrick/CSVDisplay/CSVDisplay.java
@@ -5,14 +5,29 @@ import java.util.List;
 
 import javax.swing.SwingUtilities;
 
+/**
+ * Contains main method, 
+ * 
+ * @author Patrick Heinrich
+ *
+ */
 public class CSVDisplay {
+    
+    /**
+     * @param args Arguments from the command line are dismissed
+     * @throws IOException if the path is invalid
+     */
     public static void main( String[] args ) throws IOException {
-        final List<CSVItem> itemList = CSVReader.csvToList(".\\src\\main\\resources\\Artikel_Short.csv", ';');
+        
+        final List<CSVItem> itemList = CSVHandler.csvToList(".\\src\\main\\resources\\Artikel.csv", ';');
+        
         SwingUtilities.invokeLater(new Runnable() {
+            
             @Override
             public void run() {
-                new CSVItemTable(itemList);
+                new CSVItemTable(itemList);                
             }
+            
         });
     }
 }
