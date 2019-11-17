db2173d57c56ee37eb7f6a04009d42f06273a778
Patrick Heinrich <heinrich.patrick97@web.de>
2019-11-17 15:14:10 +0100

    added jdoc

--- src/main/java/org/patrick/CSVDisplay/CSVItemTable.java
+++ src/main/java/org/patrick/CSVDisplay/CSVItemTable.java
@@ -6,10 +6,21 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+/**
+ * Extends JFrame to create a window with a table containing csv data
+ * 
+ * @author Patrick Heinrich
+ *
+ */
 public class CSVItemTable extends JFrame {
     
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Creates a JFrame table from a List of CSVItems
+     * 
+     * @param csvItemList List of CSVItems to list in the table
+     */
     public CSVItemTable(List<CSVItem> csvItemList) {
         
         CSVItemTableModel model = new CSVItemTableModel(csvItemList);
--- src/main/java/org/patrick/CSVDisplay/CSVItemTableModel.java
+++ src/main/java/org/patrick/CSVDisplay/CSVItemTableModel.java
@@ -1,10 +1,20 @@
 package org.patrick.CSVDisplay;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
+/**
+ * Creates a table model
+ * 
+ * @author Patrick Heinrich
+ *
+ */
 public class CSVItemTableModel extends AbstractTableModel{
     
     private static final long serialVersionUID = 1L;
@@ -15,30 +25,50 @@ public class CSVItemTableModel extends AbstractTableModel{
             "Grammatur","Material","Ursprungsland","Bildname"
     };
     
+    /**
+     * Constructor for the table model
+     * 
+     * @param csvItemList List of CSVItems to be listed in the table
+     */
     public CSVItemTableModel(List<CSVItem> csvItemList) {
         this.csvItemList = csvItemList;
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getColumnName(int column) {
         return columnNames[column];
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getRowCount() {
         return csvItemList.size();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getColumnCount() {
         return columnNames.length;
     }
     
+    /**
+     * Makes the cell editable
+     */
     @Override
     public boolean isCellEditable(int row, int column) {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Object getValueAt(int row, int column) {
         CSVItem tmpCSVItem = csvItemList.get(row);
@@ -59,6 +89,13 @@ public class CSVItemTableModel extends AbstractTableModel{
         return value;        
     }
     
+    /**
+     * Updates the value of a cell if it's edited
+     * 
+     * @param value New value for the cell
+     * @param row Row of the edited cell
+     * @param column Column of the edited cell
+     */
     @Override
     public void setValueAt(Object value, int row, int column) {
         CSVItem tmpCSVItem = csvItemList.get(row);
@@ -69,6 +106,11 @@ public class CSVItemTableModel extends AbstractTableModel{
                     field.setAccessible(true);
                     field.set(tmpCSVItem, (String) value);
                 } catch (IllegalArgumentException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    CSVHandler.listToCSV(csvItemList, ".\\src\\main\\resources\\Artikel_created.csv");
+                } catch (CsvDataTypeMismatchException | CsvRequiredFieldEmptyException | IOException e) {
                     e.printStackTrace();
                 }
                 break;
